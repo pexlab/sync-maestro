36fa278626abd147e0e1f71c91e145915822267c
David van der Sluijs <david@van-der-sluijs.de>
2023-05-26 13:05:34 +0200

    Update rhythm_guru.asm

--- rhythm_guru.asm
+++ rhythm_guru.asm
@@ -1,66 +1,68 @@
-.equ output_bit = 3
+// Settings for UART (e.g. if it can receive as well)
 
+//Calculate Baud rate, will be used in L and H registers
 .equ f_osc = 16_000_000
 .equ r_baud = 9600
 .equ ubrr = f_osc / (16 * r_baud)
 
-.equ b_mask = 0x18 //0x18 // 0001 1000
-.equ c_mask = 0x06 // 0000 0110
-.equ empty_mask = 0x20 //0010 0000
-.equ rcv_complete_mask = 0x80 //1000 0000
+.equ b_mask = 0x18 // 0001 1000 : enable reciever and transmitter
+.equ c_mask = 0x06 // 0000 0110 : 8 bytes per transmission
+.equ empty_mask = 0x20 // 0010 0000 : A Register - is set, if transmission is not done
+.equ rcv_complete_mask = 0x80 // 1000 0000 :
 
-sbi DDRD , output_bit
+.equ output = 2;
 
-ldi r17, byte1(ubrr)
+sbi DDRD, output
+
+//load registers
+ldi r17, byte1(ubrr) ; baud rate to H and L 
 sts UBRR0L, r17
 ldi r17, byte2(ubrr)
 sts UBRR0H, r17
-
-ldi r17, b_mask
+ldi r17, b_mask ; b_mask to B register
 sts UCSR0B, r17
+ldi r17, c_mask ; load c_mask to C register
+sts UCSR0C, r17
 
+// Registers:
+// r17 = Working register for UART settings
+// r18, r19, r20 = Counter for the interval
+// r21 = Bytes to be transmitted over UART
+// r22 = Working register for transmitting
 
-toggle:
+ldi r21, 0x00 ; init counter to 0
 
-LDI r16 , 0x80
-LDI r17 , 0x84
-LDI r18 , 0x1E
+// Main logic (called every second)
 
-ldi ZH, high(hello<<1)
-	ldi ZL, low(hello<<1)
-	char_send_loop:
-		lpm r19, Z+
-		tst r19
-		breq continue
-		call send_character
-	rjmp char_send_loop
+counter_interval:
 
-continue:
+ldi r18 , 0x92
+ldi r19 , 0xE0
+ldi r20 , 0x22
 
+cbi PORTD, output
+
+call transmit_over_uart //send counter to serial
+inc r21 //increment counter. Overflow will be ignored
+
+sbi PORTD, output
+
+// Counter logic (infinite loop to determine when to jump to the main logic)
 counter_loop:
+	subi r18 , 1
+	sbci r19 , 0
+	sbci r20 , 0
+	nop
+	nop
+brmi counter_interval 
+rjmp counter_loop
 
-; 5 Cycles
-subi r16 , 1
-sbci r17 , 0
-sbci r18 , 0
-nop
-nop
-
-BRMI toggle ; 1-2 Cycles 
-; Muss eigt noch geprüft werden ob r16 und r17 null sind
-rjmp counter_loop ; 2 Cycles
-
-send_character:
-	//pop r18 ; load char to r18
-	send_loop: ; wait until register empty
-		lds r20, UCSR0A ; load A Status register to r17
-		andi r20, empty_mask 
+// Functions which should only be called from code above
+//send a character over uart
+transmit_over_uart:
+	send_loop: //wait for the connection to be free
+		lds r22, UCSR0A
+		andi r22, empty_mask 
 	breq send_loop
-	sts UDR0, r19
+	sts UDR0, r21 //send counter
 ret
-
-hello_avr:
-	.db "hello avr",0
-
-hello:
-	.db "Hello World!",10,13, 0
