17d255da9e50ba6a5b6723e2d654cdd001850c20
David van der Sluijs <david@van-der-sluijs.de>
2023-06-16 14:22:39 +0200

    VLC

--- apps/client/src/mpv/vlc.ts
+++ apps/client/src/mpv/vlc.ts
@@ -15,16 +15,21 @@ export class Vlc {
     public readonly control = {
         
         loadFile: async ( url: string ) => {
+            console.log('playFile (loadFile)', url);
             await this.vlc.playFile( url );
+            console.log('pause (loadFile)');
             await this.vlc.pause();
         },
         
         pause_at: async ( absoluteTimeInSec: number ) => {
+            console.log('pause (pause_at)');
             await this.vlc.pause();
+            console.log('setTime (pause_at)', absoluteTimeInSec);
             await this.vlc.setTime( absoluteTimeInSec );
         },
         
         resume: async () => {
+            console.log('play (resume)');
             await this.vlc.play();
         }
     };
