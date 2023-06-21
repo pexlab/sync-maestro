df2c6933dfe2c9e8d16388e6cba7019fbc520f55
David van der Sluijs <david@van-der-sluijs.de>
2023-06-21 18:15:16 +0200

    Initial commit

--- apps/debugger/src/main.ts
+++ apps/debugger/src/main.ts
@@ -1,30 +1,37 @@
-import { SerialPort } from 'serialport'
-import process from 'process';
+import { SerialPort } from "serialport";
+import process from "process";
 
 const port = new SerialPort({
-    path    : 'COM7',
-    baudRate: 9600,
-    dataBits: 8,
-    stopBits: 1,
-    parity  : 'none',
-    highWaterMark: 1,
-    autoOpen: true
+  path: "COM7",
+  baudRate: 9600,
+  dataBits: 8,
+  stopBits: 1,
+  parity: "none",
+  highWaterMark: 1,
+  autoOpen: true,
+  xon: false,
+  xoff: false,
+  xany: true,
+  rtscts: false,
+  hupcl: false,
+  vmin: 0,
+  vtime: 0
 });
 
 let last = process.hrtime();
 
-port.on('data', (data) => {
-    
-    const byte = data[0];
-    
-    if(byte === 0x00 || byte === 0xFF){
-        return;
-    }
+port.on("data", (data) => {
 
-    const now = process.hrtime();
-    const elapsedHrtime = process.hrtime(last);
-    
-    console.log(byte + ": " + elapsedHrtime[0] * 1000 + elapsedHrtime[1] / 1e6)
-    
-    last = now;
+  const byte = data[0];
+
+  if (byte === 0x00 || byte === 0xFF) {
+    return;
+  }
+
+  const now = process.hrtime();
+  const elapsedHrtime = process.hrtime(last);
+
+  console.log(byte + ": " + elapsedHrtime[0] * 1000 + elapsedHrtime[1] / 1e6);
+
+  last = now;
 });
