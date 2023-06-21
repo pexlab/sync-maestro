5afa2cdfe5ef8b1379c0aa672a63f4a7aae2603c
Philipp Glaubitz <philipp.glaubitz@pexlab.net>
2023-06-21 17:17:38 +0200

    debugger

--- apps/debugger/src/main.ts
+++ apps/debugger/src/main.ts
@@ -1,192 +1,60 @@
-import blessed from "blessed";
-import { Subject, take } from "rxjs";
+import * as fs from 'fs';
 
-const screen = blessed.screen({
-  smartCSR: true
-});
+const serialPortPath = '/dev/tty.usbserial-AH06SQPE'; // Replace with the actual path of your serial port
 
-screen.title = "Sync-Maestro";
+let last = performance.now();
+let skip = false;
 
-const asciiArt = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣄⢸⡆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠂⢖⢘⢉⠒⡐⠁⢎⡐⠐⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢳⣽⢀⠀⡠⡀⠀⠀⠀⠀⠀⠀⠠⢎⢆⢡⡜⡡⣎⡉⢎⣊⠄⠸⡦⣄⡈⡐⠠⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣐⣔⣔⣤⣦⢹⣇⢠⣻⠁⠀⠀⠀⠀⠀⠀⢸⡣⡲⡱⡝⣼⢡⢟⣵⣫⣓⢆⠹⡦⡈⠘⢂⢈⠃⠀⠀⠀⠀⠀⠀\n" +
-  "⠳⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣹⡀⠁⢻⢗⡵⢢⡿⠀⠀⠀⠀⠀⠀⠀⢀⢏⢝⢙⣪⣊⣓⢯⢳⡿⣮⡿⣶⣮⡑⡡⠀⡌⠐⡄⠀⠀⠀⠀⠀\n" +
-  "⠀⠈⠓⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣣⠀⣘⡕⠄⣿⡇⠀⠀⠀⠀⠀⠀⠀⠚⡵⡷⢡⠽⣿⣝⣿⣳⡽⣛⢿⣳⣷⡹⡜⢤⣈⢃⠂⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡘⢐⠅⢍⡺⣟⠄⠀⠀⠀⠀⠀⠀⠀⠀⢻⣡⡝⢮⠂⢆⢄⢦⠪⡕⡯⡳⣝⢎⡎⠂⢿⡨⠂⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣔⠕⡨⣢⢿⡕⠅⠀⠀⠀⠀⠀⠀⠀⠀⣏⢯⡿⢏⠳⣜⣯⣻⣳⡺⡘⡝⠸⣟⢾⡄⠱⡱⠁⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡔⢵⣝⠿⠈⠠⠀⠀⠀⠀⠀⠀⠀⠀⡷⣽⠾⣮⡷⣹⢞⣵⢯⣟⣼⡪⣐⡛⣃⡕⠠⠨⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢽⢣⡕⡵⣀⠀⣦⡀⠀⠀⠀⠀⠀⠀⢻⡺⡗⢷⢞⢷⡻⣮⡿⣯⣿⢧⣿⠊⡉⢎⢀⠇⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡁⠳⣵⣝⢮⣳⣾⣟⣞⢦⡀⠀⠀⠀⠀⠈⣷⣪⢷⡽⣯⣿⣾⢿⣟⣿⡝⡧⢢⠢⠐⡌⠀⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠨⡲⣻⣳⣿⣿⣾⡿⣷⣽⣣⣄⠀⠀⠀⠈⢳⡟⣾⣻⡷⣿⢿⣿⣻⣮⡻⠎⠓⢘⢀⠀⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠤⣷⣔⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢾⣿⣿⣻⣿⣿⣿⣿⣕⢿⣦⣄⠀⠀⠀⠹⣳⣽⣟⡿⣻⢝⡵⣃⣴⢖⡿⣩⣫⣣⠀⠀⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣻⣥⠹⣮⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢹⣿⣿⣯⣿⣿⣿⣿⣞⣿⣞⣧⠀⠀⢀⢏⡘⢏⠮⣳⢝⡾⣯⣯⢯⣾⣵⡳⣗⣟⣶⣄⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢎⣴⣯⣅⢻⣇⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⢿⣿⣾⣷⣿⡯⣆⠔⣕⣙⢎⢎⣷⡿⣿⣺⣻⣳⣷⣟⡿⣿⣮⢷⣿⣧⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠲⣿⡖⣿⡨⠥⡈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⡺⣻⠴⡧⣫⡿⣯⣟⢿⣷⣻⣟⣿⣮⢿⣯⡻⣷⣯⣻⢧⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣾⢑⡿⣖⢌⠢⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣳⣯⣺⡪⣯⢻⣿⡼⣯⡿⣿⣻⣿⣟⣿⣿⡳⡄\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⢻⢮⣻⣽⡢⡣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣻⣿⣿⣾⣿⣽⣿⣮⢷⣯⢺⡵⣿⡯⣿⣷⢿⣷⣻⣿⢯⣿⡝⣦\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣝⢷⢽⡮⡃⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣹⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣯⣿⣿⣝⣿⣝⡿⣿⣯⢿⣿⣟⣿⣟⣮⢷\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢴⢋⡏⣎⢺⡢⡀⣼⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡽⣽⠻⣿⣿⣿⣿⣿⣿⣿⢿⣯⣿⣞⡾⣷⣝⣿⣻⣞⣿⣿⣿⡿⣝⡯⣷\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡃⡑⢵⡱⣝⢮⣾⣿⣿⣽⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⢯⡾⠃⢠⣿⣿⣷⣿⣿⣷⣿⣿⣿⣿⣾⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣟⡗\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠸⡸⣣⣿⣿⣿⣿⣿⣿⣯⡻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣳⡿⠁⣰⣿⣿⣿⣽⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣾⣮⣾⠃\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠻⣿⣿⣿⣿⣿⣿⣿⣿⡾⣻⢦⡀⠀⠀⠀⠀⣀⣾⣿⡿⣵⡿⠁⣼⣿⣿⣿⣿⣽⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣯⣻⣽⡝⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣟⣿⣿⣝⣯⢖⣤⣿⣿⣿⣿⢿⣽⡟⠃⣼⣿⣿⣿⣿⣯⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⠃⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣾⣟⣿⣿⣿⣿⣻⡿⡿⠀⣼⣿⣿⣿⣿⣿⣽⣿⣻⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⡻⣿⡇⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣯⣿⣿⣻⣿⣯⣿⡿⠁⢰⣿⣿⣿⣿⣿⣯⣿⣻⣿⣿⣿⣿⡿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣮⣿⣟⣽⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣻⡏⠀⠀⣿⣿⣿⡿⣷⣿⣾⢿⣿⢿⣾⣿⣽⣿⣿⣿⡿⣿⣿⢿⣿⣿⣿⣿⣿⣮⠃⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⠏⢀⠈⠀⣿⣿⣽⣿⣻⣷⣿⣿⣿⣿⣿⣯⣿⣾⣿⣿⣿⡷⡽⣿⣻⣿⣿⢿⣿⡗⠀⠀⠀⠀\n" +
-  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⠃⠄⡀⠂⠀⣿⡿⣷⣿⣿⣽⣿⣷⣿⣿⣾⢿⣾⣷⣿⣿⣿⣿⣯⡺⣿⣽⣿⣿⠟⠀⠀⠀⠀⠀";
-
-const logoArt = " ,-.                  .   ,             .          \n" +
-  "(   `                 |\\ /|             |          \n" +
-  " `-.  . . ;-. ,-. --- | V | ,-: ,-. ,-. |-  ;-. ,-.\n" +
-  ".   ) | | | | |       |   | | | |-' `-. |   |   | |\n" +
-  " `-'  `-| ' ' `-'     '   ' `-` `-' `-' `-' '   `-'\n" +
-  "      `-'                                          \n";
-
-const background = blessed.box({
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  style: {
-    bg: "#0000ff"
-  }
-});
-
-const art = blessed.box({
-  bottom: 0,
-  right: 0,
-  width: "shrink",
-  height: "shrink",
-  content: asciiArt,
-  style: {
-    fg: "white",
-    bg: "blue"
-  }
-});
-
-const logo = blessed.box({
-  left: "center",
-  top: 1,
-  width: "shrink",
-  height: "shrink",
-  content: logoArt,
-  style: {
-    fg: "white",
-    bg: "blue"
-  }
-});
-
-const consoleWindow = blessed.box({
-  top: 9,
-  left: 3,
-  width: "60%-2",
-  height: "100%-13",
-  content: "Hello, world!",
-  style: {
-    fg: "white",
-    bg: "black"
-  },
-  padding: 1,
-  keys: true,
-  mouse: true,
-  scrollable: true,
-  focusable: true,
-  scrollbar: {
-    style: {
-      bg: "white"
+fs.open(serialPortPath, fs.constants.O_RDONLY | fs.constants.O_NONBLOCK, (err, fd) => {
+    if (err) {
+        console.error('Error opening serial port:', err);
+        return;
     }
-  }
+    
+    const readFromSerial = () => {
+        const buffer = Buffer.alloc(1);
+        
+        fs.read(fd, buffer, 0, 1, null, (err, bytesRead) => {
+            if (err) {
+                //console.error('Error reading from serial port:', err);
+                readFromSerial();
+                
+                return;
+            }
+            
+            if (bytesRead > 0) {
+                // Process the received byte
+                
+                const b = buffer.slice(0, bytesRead)[0];
+                
+                if(b === 0x00){
+                    skip = true;
+                    readFromSerial();
+                    return;
+                }
+                
+                if(b === 0xFF){
+                    skip = false;
+                    readFromSerial();
+                    return;
+                }
+                
+                if(skip){
+                    readFromSerial();
+                    return;
+                }
+                
+                const now = performance.now();
+                
+                console.log(b + ": " + (now-last))
+                
+                last = now;
+            }
+            
+            // Continue reading from the serial port
+            readFromSerial();
+        });
+    };
+    
+    readFromSerial(); // Start reading from the serial port
 });
-
-const list = blessed.list({
-  top: 9,
-  left: 3,
-  width: "60%-2",
-  height: "100%-13",
-  keys: true,
-  mouse: true,
-  content: "{center}Console{/center}",
-  style: {
-    selected: {
-      bg: "green",
-      fg: "black"
-    }
-  }
-});
-
-list.hide();
-
-const label = blessed.box({
-  parent: screen,
-  top: 8,
-  left: 2,
-  width: "60%",
-  height: "100%-11",
-  content: "",
-  tags: true,
-  style: {
-    fg: "black",
-    bg: "#939393"
-  },
-  shadow: true,
-  focusable: false
-});
-
-const optionSelected = new Subject<string>();
-
-list.on("select", (item, index) => {
-  optionSelected.next(item.getText());
-});
-
-screen.append(background);
-screen.append(art);
-screen.append(logo);
-screen.append(label);
-screen.append(list);
-screen.append(consoleWindow);
-
-consoleWindow.hide();
-
-export const askList = (prompt: string, options: [string, string][]) => {
-
-  return new Promise<string>((resolve) => {
-
-    list.show();
-    consoleWindow.hide();
-
-    label.setContent("{center}" + prompt + "{/center}");
-
-    list.clearItems();
-    list.setItems(options.map((option) => option[0]));
-
-    optionSelected.pipe(take(1)).subscribe((option) => {
-
-      list.hide();
-      consoleWindow.show();
-
-      label.setContent("{center}Console{/center}");
-
-      consoleWindow.focus();
-
-      screen.render();
-
-      resolve(options.find(o => o[0] === option)![1]);
-    });
-
-    list.focus();
-
-    screen.render();
-  });
-};
-
-export const log = (text: string) => {
-  consoleWindow.pushLine(text);
-  screen.render();
-};
-
-screen.key(["q", "C-c"], function(ch, key) {
-  return process.exit(0);
-});
-
-screen.render();
